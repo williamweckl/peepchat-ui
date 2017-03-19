0b5ad7ace7e3bd2ad3ea75f06697e622add0cfe8
William Weckl <william.weckl@gmail.com>
2017-03-18 21:36:55 -0300

    auth pages

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,15 @@
-/* eslint-env node */
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        'bower_components/materialize/sass'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
